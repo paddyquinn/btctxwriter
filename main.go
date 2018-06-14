88c24781c500b6b5de7378d813077c100427bbf6
Padraic Quinn <pmq2101@columbia.edu>
2018-06-14 16:48:48 -0400

    Write outline for HTLC P2SH function

--- main.go
+++ main.go
@@ -14,35 +14,33 @@ import (
   "github.com/btcsuite/btcutil"
 )
 
-// gist = 'https://gist.github.com/paddyquinn/7277b010eaad0035c436a398c4cf0d43'
-
 func main() {
   b := new(big.Int)
   b.SetString(os.Args[1], 10)
-  privateKey, publicKey := btcec.PrivKeyFromBytes(btcec.S256(), b.Bytes())
+  privKey, pubKey := btcec.PrivKeyFromBytes(btcec.S256(), b.Bytes())
   serializedScriptPubKey, _ := hex.DecodeString("76a9147cb9c22532e25baa608e93877fc1b67d7c11bda188ac")
-  dataToSign := bytes.NewBuffer(serializeTx(publicKey, serializedScriptPubKey))
+  dataToSign := bytes.NewBuffer(serializeTx(pubKey, serializedScriptPubKey))
   serializedSigHash := make([]byte, 4)
   binary.LittleEndian.PutUint32(serializedSigHash, uint32(txscript.SigHashAll))
   dataToSign.Write(serializedSigHash)
   sha256Hash := sha256.Sum256(dataToSign.Bytes())
   doubleSHA256 := sha256.Sum256(sha256Hash[:])
-  sig, _ := privateKey.Sign(doubleSHA256[:])
+  sig, _ := privKey.Sign(doubleSHA256[:])
   serializedSig := sig.Serialize()
   // TODO: comment add byte for sighash
   buffer := bytes.NewBuffer([]byte{byte(len(serializedSig)+1)})
   buffer.Write(serializedSig)
   buffer.WriteByte(byte(txscript.SigHashAll))
-  serializedPubKey := publicKey.SerializeCompressed()
+  serializedPubKey := pubKey.SerializeCompressed()
   buffer.WriteByte(byte(len(serializedPubKey)))
   buffer.Write(serializedPubKey)
-  fmt.Printf("%x\n", serializeTx(publicKey, buffer.Bytes()))
+  fmt.Printf("%x\n", serializeTx(pubKey, buffer.Bytes()))
   /*
   def redeem_transaction():
   print('to do')*/
 }
 
-func serializeTx(publicKey *btcec.PublicKey, serializedScriptSig []byte) []byte {
+func serializeTx(pubKey *btcec.PublicKey, serializedScriptSig []byte) []byte {
   // Serialize the transaction version as 4 little endian bytes.
   var version uint32 = 1
   serializedVersion := make([]byte, 4)
@@ -58,13 +56,13 @@ func serializeTx(publicKey *btcec.PublicKey, serializedScriptSig []byte) []byte 
 
   // Serialize the number of output UTXOs to be created. In our case it is 2: 1 for the challenge and 1 as a change
   // address.
-  // TODO: make this 2
-  serializedNumOutputs := []byte{1}
+  serializedNumOutputs := []byte{2}
   serializedTx.Write(serializedNumOutputs)
 
-  // TODO: serialize the challenge UTXO
+  // TODO: write comment (total = 2800000)
+  serializedTx.Write(serializeOutput(100000, p2sh(pubKey, pubKey)))
 
-  serializedTx.Write(serializeOutput(publicKey))
+  serializedTx.Write(serializeOutput(2600000, p2pkh(pubKey)))
 
   // Serialize the locktime as 4 little endian bytes. Use 0 so the transaction can be mined immediately.
   var locktime uint32 = 0
@@ -106,30 +104,51 @@ func serializeInput(serializedScriptSig []byte) []byte {
   return serializedInput.Bytes()
 }
 
-func serializeOutput(publicKey *btcec.PublicKey) []byte {
-  // Serialize the amount of change as 8 little endian bytes.
-  var change uint64 = 2800000
-  serializedChange := make([]byte, 8)
-  binary.LittleEndian.PutUint64(serializedChange, change)
-  serializedOutput := bytes.NewBuffer(serializedChange)
-
-  // Serialize a P2PKH script pubkey.
-  scriptPubKey := p2pkh(publicKey)
+func serializeOutput(amount uint64, serializedScriptPubKey []byte) []byte {
+  // Serialize the amount as 8 little endian bytes.
+  serializedAmount := make([]byte, 8)
+  binary.LittleEndian.PutUint64(serializedAmount, amount)
+  serializedOutput := bytes.NewBuffer(serializedAmount)
 
   // TODO: make note about assumption that length will be less than 0xfd
-  serializedOutput.WriteByte(byte(len(scriptPubKey)))
+  serializedOutput.WriteByte(byte(len(serializedScriptPubKey)))
 
-  serializedOutput.Write(scriptPubKey)
+  serializedOutput.Write(serializedScriptPubKey)
 
   return serializedOutput.Bytes()
 }
 
-// TODO: make a comment that this is unnecessary
-func p2pkh(publicKey *btcec.PublicKey) []byte {
+// TODO: make a comment that this is unnecessary; may not be if we have to send a tx to the purse public key
+func p2pkh(pubKey *btcec.PublicKey) []byte {
   buffer := new(bytes.Buffer)
-  buffer.Write(btcutil.Hash160(publicKey.SerializeCompressed()))
-  publicKeyHash := buffer.Bytes()
-  scriptPubKey := []byte{txscript.OP_DUP, txscript.OP_HASH160, byte(len(publicKeyHash))}
-  scriptPubKey = append(scriptPubKey, publicKeyHash...)
+  buffer.Write(btcutil.Hash160(pubKey.SerializeCompressed()))
+  pubKeyHash := buffer.Bytes()
+  scriptPubKey := []byte{txscript.OP_DUP, txscript.OP_HASH160, byte(len(pubKeyHash))}
+  scriptPubKey = append(scriptPubKey, pubKeyHash...)
   return append(scriptPubKey, txscript.OP_EQUALVERIFY, txscript.OP_CHECKSIG)
+}
+
+func p2sh(pubKey *btcec.PublicKey, refundPubKey *btcec.PublicKey) []byte {
+  redeemScriptBuffer := bytes.NewBuffer([]byte{txscript.OP_HASH160})
+  gist := []byte("https://gist.github.com/paddyquinn/be53db32330089c7b7ae5b7ce4353bbc")
+  gistHash := btcutil.Hash160(gist)
+  redeemScriptBuffer.WriteByte(byte(len(gistHash)))
+  redeemScriptBuffer.Write(gistHash)
+  redeemScriptBuffer.Write([]byte{txscript.OP_EQUAL, txscript.OP_IF})
+  serializedPubKey := pubKey.SerializeCompressed()
+  redeemScriptBuffer.WriteByte(byte(len(serializedPubKey)))
+  redeemScriptBuffer.Write(serializedPubKey)
+  redeemScriptBuffer.Write([]byte{txscript.OP_ELSE})
+  //buffer.Write(timelock)
+  redeemScriptBuffer.Write([]byte{txscript.OP_CHECKSEQUENCEVERIFY, txscript.OP_DROP})
+  serializedRefundPubKey := refundPubKey.SerializeCompressed()
+  redeemScriptBuffer.WriteByte(byte(len(serializedRefundPubKey)))
+  redeemScriptBuffer.Write(serializedRefundPubKey)
+  redeemScriptBuffer.Write([]byte{txscript.OP_ENDIF, txscript.OP_CHECKSIG})
+  redeemScript := redeemScriptBuffer.Bytes()
+  buffer := bytes.NewBuffer([]byte{txscript.OP_HASH160})
+  buffer.WriteByte(byte(len(redeemScript)))
+  buffer.Write(redeemScript)
+  buffer.WriteByte(txscript.OP_EQUAL)
+  return buffer.Bytes()
 }
